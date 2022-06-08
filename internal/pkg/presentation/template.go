0be23b7a2a9f7acf630c41b32e3b0505b04110fb
Mikael Nordberg <mikael.nordberg@masarinconsulting.se>
2022-06-08 13:58:26 +0200

    logo

--- internal/pkg/presentation/template.go
+++ internal/pkg/presentation/template.go
@@ -34,9 +34,8 @@ const templ string = `
 
 <body>
   <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Diwise</a>
-    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
-      data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#"><img src="./assets/brand/diwise-white-alpha+tight.png" style="height:30px"></a>
+    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="navbar-nav">
