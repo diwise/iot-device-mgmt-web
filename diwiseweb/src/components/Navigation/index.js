3777a50d35689c4bc88e15c26ee195435c443d1b
Deliciaes <102973683+eramstedt@users.noreply.github.com>
2022-06-28 11:33:29 +0200

    theme toggle now works

--- diwiseweb/src/components/Navigation/index.js
+++ diwiseweb/src/components/Navigation/index.js
@@ -2,8 +2,10 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import "./navigation.css";
 import ThemeButton from "../ThemeButton";
+import userIcon from "../../assets/icons/user.svg";
 
 function MainNav() {
   return (
@@ -84,18 +86,26 @@ function MainNav() {
                       <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
                     </svg>
                   </Nav.Link>
-                  <Nav.Link href="/">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="30"
-                      height="30"
-                      fill="currentColor"
-                      className="bi bi-person"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
-                    </svg>
-                  </Nav.Link>
+                  <NavDropdown
+                    title={<img src={userIcon} />}
+                    id="basic-nav-dropdown"
+                  >
+                    hej
+                    <NavDropdown.Item href="#action/3.1">
+                      Action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">
+                      Something
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">
+                      Separated link
+                    </NavDropdown.Item>
+                  </NavDropdown>
+
                   <ThemeButton />
                 </Nav>
               </Offcanvas.Body>
--- diwiseweb/src/components/ThemeButton/index.js
+++ diwiseweb/src/components/ThemeButton/index.js
@@ -1,26 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import { useState, useEffect } from "react";
 import "./themebutton.css";
 
 function ThemeButton() {
-  const bodyTheme = document.body;
-  const savedMode = JSON.parse(localStorage.getItem("dark"));
-  const [darkTheme, setDarkTheme] = useState(getInitialTheme());
+  const [darkMode, setDarkMode] = useState(getInitialMode());
   useEffect(() => {
-    localStorage.setItem("dark", JSON.stringify(darkTheme));
-  }, [darkTheme]);
+    localStorage.setItem("dark", JSON.stringify(darkMode));
+  }, [darkMode]);
 
-  function getInitialTheme() {
+  function getInitialMode() {
     const isReturningUser = "dark" in localStorage;
+    const savedMode = JSON.parse(localStorage.getItem("dark"));
     const userPrefersDark = getPrefColorScheme();
-
-    if (isReturningUser) {
-      bodyTheme.classList.add("darkTheme");
-      bodyTheme.classList.remove("lightTheme");
-    } else {
-      bodyTheme.classList.remove("darkTheme");
-      bodyTheme.classList.add("lightTheme");
-    }
-
     // if mode was saved --> dark / light
     if (isReturningUser) {
       return savedMode;
@@ -35,53 +27,132 @@ function ThemeButton() {
   }
 
   function getPrefColorScheme() {
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      return true;
-    } else {
-      return false;
-    }
+    if (!window.matchMedia) return;
+
+    return window.matchMedia("(prefers-color-scheme: dark)").matches;
   }
 
   return (
-    <>
-      <button
-        className="themeButton"
-        onClick={() => {
-          setDarkTheme((prevTheme) => !prevTheme);
-          if (!darkTheme) {
-            bodyTheme.classList.add("darkTheme");
-            bodyTheme.classList.remove("lightTheme");
-          } else {
-            bodyTheme.classList.remove("darkTheme");
-            bodyTheme.classList.add("lightTheme");
-          }
-        }}
-      >
-        {darkTheme ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="25"
-            height="25"
-            fill="white"
-            className="bi bi-brightness-high-fill"
-            viewBox="0 0 16 16"
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+      <nav>
+        <div className="toggle-container">
+          <span style={{ color: darkMode ? "grey" : "yellow" }}>☀︎</span>
+          <span className="toggle">
+            <input
+              checked={darkMode}
+              onChange={() => setDarkMode((prevMode) => !prevMode)}
+              id="checkbox"
+              className="checkbox"
+              type="checkbox"
+            />
+            <label htmlFor="checkbox" />
+          </span>
+
+          <span
+            style={{
+              color: darkMode
+                ? document.body.classList.add("darkTheme") +
+                  document.body.classList.remove("lightTheme")
+                : document.body.classList.add("lightTheme") +
+                  document.body.classList.remove("darkTheme"),
+            }}
           >
-            <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="25"
-            height="25"
-            fill="black"
-            className="bi bi-moon"
-            viewBox="0 0 16 16"
-          >
-            <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z" />
-          </svg>
-        )}
-      </button>
-    </>
+            ☾
+          </span>
+        </div>
+      </nav>
+      <main></main>
+    </div>
   );
 }
 export default ThemeButton;
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
+
+// import { useState, useEffect } from "react";
+// import "./themebutton.css";
+
+// function ThemeButton() {
+//   const bodyTheme = document.body;
+//   const savedMode = JSON.parse(localStorage.getItem("dark"));
+//   const [darkTheme, setDarkTheme] = useState(getInitialTheme());
+//   useEffect(() => {
+//     localStorage.setItem("dark", JSON.stringify(darkTheme));
+//   }, [darkTheme]);
+
+//   function getInitialTheme() {
+//     const isReturningUser = "dark" in localStorage;
+//     const userPrefersDark = getPrefColorScheme();
+
+//     // if mode was saved --> dark / light
+//     if (isReturningUser) {
+//       return savedMode;
+//       // if preferred color scheme is dark --> dark
+//     } else if (userPrefersDark) {
+//       return true;
+//       // otherwise --> light
+//     } else {
+//       return false;
+//     }
+//     // return savedMode || false;
+//   }
+
+//   function getPrefColorScheme() {
+//     if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+//       bodyTheme.classList.remove("lightTheme");
+//       bodyTheme.classList.add("darkTheme");
+//       return true;
+//     } else {
+//       bodyTheme.classList.remove("darkTheme");
+//       bodyTheme.classList.add("lightTheme");
+//       return false;
+//     }
+//   }
+
+//   return (
+//     <>
+//       <button
+//         className="themeButton"
+//         onClick={() => {
+//           setDarkTheme((prevTheme) => !prevTheme);
+//           console.log(getInitialTheme());
+//           if (!getInitialTheme()) {
+//             console.log("light");
+//             bodyTheme.classList.remove("darkTheme");
+//             bodyTheme.classList.add("lightTheme");
+//           } else {
+//             console.log("dark");
+//             bodyTheme.classList.add("darkTheme");
+//             bodyTheme.classList.remove("lightTheme");
+//           }
+//         }}
+//       >
+//         {darkTheme ? (
+//           <svg
+//             xmlns="http://www.w3.org/2000/svg"
+//             width="25"
+//             height="25"
+//             fill="white"
+//             className="bi bi-brightness-high-fill"
+//             viewBox="0 0 16 16"
+//           >
+//             <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+//           </svg>
+//         ) : (
+//           <svg
+//             xmlns="http://www.w3.org/2000/svg"
+//             width="25"
+//             height="25"
+//             fill="black"
+//             className="bi bi-moon"
+//             viewBox="0 0 16 16"
+//           >
+//             <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z" />
+//           </svg>
+//         )}
+//       </button>
+//     </>
+//   );
+// }
+// export default ThemeButton;
