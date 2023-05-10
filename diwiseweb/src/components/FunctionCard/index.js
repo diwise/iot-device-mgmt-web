f5c4342fff9aaaf51198d6ef6d3ade1d33d5d8dc
shestyf <sofia.styf@masarinconsulting.se>
2023-05-10 10:32:16 +0200

    kW on building card unit instead of W

--- diwiseweb/src/components/FunctionCard/index.js
+++ diwiseweb/src/components/FunctionCard/index.js
@@ -88,7 +88,7 @@ const PresenceCard = ({ func }) => {
 
 const BuildingCard = ({ func }) => {
     return (
-        <LineCard f={func} titleText={func.building.power + "W"} label={""} lastN="100" />
+        <LineCard f={func} titleText={func.building.power + "kW"} label={""} lastN="100" />
     );
 };
 
